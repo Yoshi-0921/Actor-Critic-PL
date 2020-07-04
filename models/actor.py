040b5f801823cfe5b2d0aa1af539eb7f9c50fc02
Yoshi0921 <narumoto16@gmail.com>
2020-07-04 17:36:53 +0900

    Second commit

--- models/actor.py
+++ models/actor.py
@@ -1,12 +1,26 @@
-import numpy as np
-import torch
+# -*- coding: utf-8 -*-
+
 import torch.nn as nn
 import torch.nn.functional as F
-import pytorch_lightning as pl
+from torch.distributions import Categorical
+
 
 class Actor(nn.Module):
-    def __init__(self):
-        super(Actor, self).__init__()
+    """
+    This is MLP which outputs the stochastic distribution of actions
+    """
 
-    def forward(self, x):
-        pass
+    def __init__(self, state_size, action_size):
+        super(Actor, self).__init__()
+        self.state_size = state_size
+        self.action_size = action_size
+        self.linear1 = nn.Linear(self.state_size, 128)
+        self.linear2 = nn.Linear(128, 256)
+        self.linear3 = nn.Linear(256, self.action_size)
+
+    def forward(self, state):
+        output = F.relu(self.linear1(state))
+        output = F.relu(self.linear2(output))
+        output = self.linear3(output)
+        distribution = Categorical(F.softmax(output, dim=-1))
+        return distribution
